4e3ff4b5cf677c34c0fb651c3b326e22c935f33d
asminkarki012 <asminkarki09@gmail.com>
2024-11-07 08:53:38 +0545

    cleanup

--- setup_config.sh
+++ setup_config.sh
@@ -45,6 +45,8 @@ setup_neovim_config() {
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
+    #clean up files
+    rm lazygit.tar.gz lazygit
   fi
 
 }
