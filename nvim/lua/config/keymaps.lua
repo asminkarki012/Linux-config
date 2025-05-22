beaa21595e563d1d664bd1630bc05852b044ab10
asminkarki012 <37731845+asminkarki012@users.noreply.github.com>
2025-05-22 11:18:40 +0545

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,16 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>sx",
+--   require("telescope.builtin").resume,
+--   { noremap = true, silent = true, desc = "Resume" }
+-- )
+
 
 vim.keymap.set(
-  "i",          
-  "jk",         
-  "<Esc>",      
+  "i",          -- Insert mode
+  "jk",         -- Keys to press
+  "<Esc>",      -- Action to take (Escape)
   { noremap = true, silent = true, desc = "Map jk to Esc in insert mode" }
 )
